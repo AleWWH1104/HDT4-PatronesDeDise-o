ab2f71e9051fb905ce3cdb9efd2e248e8bfaa88e
Iris <irisale.ayala@gmail.com>
2024-02-21 22:24:42 -0600

    prueba unitaria para la calculadora

--- PatronesDeDiseno/src/test/java/com/ejemplo/calcuTest.java
+++ PatronesDeDiseno/src/test/java/com/ejemplo/calcuTest.java
@@ -1,5 +1,27 @@
 package com.ejemplo;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class calcuTest {
+@Test
+    public void testPostfixCalc() {
+        // Crear una instancia de la clase PostfixCalc
+        PostfixCalc<Integer> calculator = PostfixCalc.getInstance();
+
+        // Crear una instancia de la pila (puedes utilizar un ArrayList, Vector u otra implementación)
+        IStack<Integer> stack = new aArrayList<>();
+
+        // Definir la expresión postfix que deseas evaluar
+        String postfixExpression = "58+";
+
+        // Evaluar la expresión postfix
+        Integer result = calculator.poFixcalc(postfixExpression, stack);
+
+        // Verificar si el resultado es el esperado
+        // Para la expresión "5 2 +" el resultado debería ser 7
+        assertEquals(Integer.valueOf(13), result);
+    }
     
 }
